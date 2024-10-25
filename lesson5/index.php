b137281fa67dd98a0cf5446c114d55bfc99bef6a
Alexey <alexey.anasov@yandex.by>
2024-10-25 23:30:42 +0300

    add lesson5

---
+++ lesson5/index.php
@@ -0,0 +1,8 @@
+<!--<form action="то чем проверяем" method="Get/post">
+    <input name="login" type="text">
+    <input name="password" type="password">
+    <input type="submit">
+</form>-->
+<?php
+
+?>
