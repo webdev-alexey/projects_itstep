b8ac11b79fa8117a7a71b2cc7d94178c99658eaf
Alexey <alexey.anasov@yandex.by>
2024-11-27 21:35:51 +0300

    add simple-blog.loc

---
+++ simple-blog.loc/models/articles.php
@@ -0,0 +1,23 @@
+<!--Для работы со статьями функции-->
+<?php
+    /*вывод всех статей*/
+    function articles_all(){
+        
+    }
+    /*получение конкретной статьи по айди*/
+    function articles_get($id){
+        
+    }
+    /*создание статьи*/
+    function articles_new($title, $date, $content){
+        
+    }
+    /*редактирование статьи*/
+    function articles_edit($id, $title, $date, $content){
+        
+    }
+    /*удаление статьи*/
+    function articles_delete($id){
+        
+    }
+?>
